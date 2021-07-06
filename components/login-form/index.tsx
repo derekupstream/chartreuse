b23b07eab108255b9f3b7db9f1be331817eca7f7
Reinaldo Schiehll <rn.schiehll@gmail.com>
2021-07-06 04:43:56 -0300

    add login page

---
+++ components/login-form/index.tsx
@@ -0,0 +1,48 @@
+import { Form, Input, Button } from "antd";
+import * as S from "./styles";
+
+export default function LoginForm() {
+  const onFinish = (values: any) => {
+    console.log({ values });
+  };
+
+  return (
+    <S.Wrapper>
+      <S.Title>Login</S.Title>
+      <S.LoginForm
+        name="login"
+        layout="vertical"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              type: "email",
+              message: "Please input a valid email!",
+            },
+          ]}
+        >
+          <Input type="email" />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" block>
+            Submit
+          </Button>
+        </Form.Item>
+      </S.LoginForm>
+    </S.Wrapper>
+  );
+}
