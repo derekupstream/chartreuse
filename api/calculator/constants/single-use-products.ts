a21364dec8343275fd39041b6d8dabeb500405fb
Matt Casey <mattwad@gmail.com>
2021-09-03 22:36:31 -0600

    fix ts error

--- api/calculator/constants/single-use-products.ts
+++ api/calculator/constants/single-use-products.ts
@@ -3,7 +3,7 @@
 import { SingleUseProduct } from '../types/products';
 
 const products: SingleUseProduct[] = [
-  { id: 1, boxWeight: 1.3, itemWeight: 0, unitsPerCase: 1000, primaryMaterial: 'Aluminum', primaryMaterialWeightPerUnit: 0.1, secondaryMaterial: 'Aluminum', secondaryMaterialWeightPerUnit: 0.1 }
+  { id: 1, boxWeight: 1.3, category: 1, type: 1, itemWeight: 0, unitsPerCase: 1000, primaryMaterial: 'Aluminum', primaryMaterialWeightPerUnit: 0.1, secondaryMaterial: 'Aluminum', secondaryMaterialWeightPerUnit: 0.1 }
 ];
 
 export function getProductById (id: number) {
