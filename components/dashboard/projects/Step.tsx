2745f02a640451ac458d8cf289dd3b3d3d0629b5
Chris Roth <chris@cjroth.com>
2021-10-24 15:06:40 +0300

    fix typo

--- components/dashboard/projects/Step.tsx
+++ components/dashboard/projects/Step.tsx
@@ -1,39 +1,39 @@
-import Setup from "./steps/setup";
-import SingleUsePurchasing from "./steps/single-use";
-import { DashboardUser } from "components/dashboard";
-import { Project } from "@prisma/client";
+import Setup from './steps/setup'
+import SingleUsePurchasing from './steps/single-use'
+import { DashboardUser } from 'components/dashboard'
+import { Project } from '@prisma/client'
 
 /* eslint-disable react/display-name */
 export const stepsMap = {
   SETUP: Setup,
   SINGLE_USE: SingleUsePurchasing,
   RESUSABLE: () => <div>Reusable purchasing</div>,
-  ADITIONAL_COSTS: () => <div>Aditional costs</div>,
+  ADDITIONAL_COSTS: () => <div>Additional costs</div>,
   SAVING_PROJECTIONS: () => <div>Saving projections</div>,
-};
+}
 
-const STEPS = Object.keys(stepsMap) as (keyof typeof stepsMap)[];
+const STEPS = Object.keys(stepsMap) as (keyof typeof stepsMap)[]
 
 export const getStepByIndex = (index: number) => {
-  return STEPS[index];
-};
+  return STEPS[index]
+}
 
 export const getStepIndex = (step: string) => {
-  return STEPS.findIndex((s) => s === step);
-};
+  return STEPS.findIndex(s => s === step)
+}
 
 export type StepProps = {
-  step: keyof typeof stepsMap;
-  user: DashboardUser;
-  project?: Project;
-  onComplete: (id?: string) => void;
-};
+  step: keyof typeof stepsMap
+  user: DashboardUser
+  project?: Project
+  onComplete: (id?: string) => void
+}
 
 const Step = ({ step, user, ...props }: StepProps) => {
-  const StepComponent = stepsMap[step as keyof typeof stepsMap];
+  const StepComponent = stepsMap[step as keyof typeof stepsMap]
 
   // @ts-ignore - this is a hack because project is sometimes required, sometimes not
-  return StepComponent ? <StepComponent {...props} user={user} /> : null;
-};
+  return StepComponent ? <StepComponent {...props} user={user} /> : null
+}
 
-export default Step;
+export default Step
