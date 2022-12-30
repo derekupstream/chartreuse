d2d8bc93bad9b7efa271795568f77b5ca0e8492d
Matt Casey <mattwad@gmail.com>
2022-12-30 00:40:47 -0700

    upgrade mailgun sdk to v8 (#128)
    * upgrade mailgun sdk
    
    * fix test
    
    * undo change

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
+  transformIgnorePatterns: [
+    // neat-csv is an esm module, so we want it to be trasnformed
+    'node_modules/(?!neat-csv/.*)',
+  ],
 }
