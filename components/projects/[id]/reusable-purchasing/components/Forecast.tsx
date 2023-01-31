2f03d73f67cadb66860989b1ab410dfb1456c66f
Matt Casey <mattwad@gmail.com>
2023-01-31 15:51:20 -0700

    add change to reusables ui (#141)

--- components/projects/[id]/reusable-purchasing/components/Forecast.tsx
+++ components/projects/[id]/reusable-purchasing/components/Forecast.tsx
@@ -13,18 +13,22 @@ interface Props {
 const Forecast: FC<Props> = ({ item }) => {
   const oneTimeCost = item.caseCost * item.casesPurchased;
   const annualCost = oneTimeCost * item.annualRepurchasePercentage;
+  const change = -1 * (oneTimeCost - annualCost);
 
   return (
     <InfoCard theme='forecast'>
       <Row>
-        <Col span={17}>
+        <Col span={14}>
           <Typography.Title level={5}>Repurchase Forecast</Typography.Title>
         </Col>
-        <Col span={7}>
+        <Col span={5}>
           <SmallText>Total</SmallText>
         </Col>
+        <Col span={5}>
+          <SmallText>Change</SmallText>
+        </Col>
       </Row>
-      <Row>
+      {/* <Row>
         <Col span={17}>
           <SmallText>Annual rate</SmallText>
         </Col>
@@ -33,18 +37,23 @@ const Forecast: FC<Props> = ({ item }) => {
             <strong>{`${round(item.annualRepurchasePercentage * 100)}%`}</strong>
           </Typography.Text>
         </Col>
-      </Row>
+      </Row> */}
       <Row>
-        <Col span={17}>
+        <Col span={14}>
           <SmallText>Annual cost</SmallText>
           <br />
           <SmallerText>
-            ({formatToDollar(item.caseCost)}/case x {item.casesPurchased * item.annualRepurchasePercentage})
+            ({formatToDollar(item.caseCost)}/case x {round(item.casesPurchased * item.annualRepurchasePercentage)})
           </SmallerText>
         </Col>
-        <Col span={7}>
+        <Col span={5}>
           <Typography.Text>
             <strong>{formatToDollar(annualCost)}</strong>
+          </Typography.Text>
+        </Col>
+        <Col span={5}>
+          <Typography.Text>
+            <strong>{formatToDollar(change)}</strong>
           </Typography.Text>
         </Col>
       </Row>
