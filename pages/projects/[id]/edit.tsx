21784ab34e2f5a558c9ed93aad782222c5723275
Matt Casey <mattwad@gmail.com>
2023-01-31 01:26:58 -0700

    add back button for editing project

--- pages/projects/[id]/edit.tsx
+++ pages/projects/[id]/edit.tsx
@@ -4,6 +4,7 @@ import type { GetServerSideProps } from 'next';
 import type { DashboardUser } from 'components/dashboard';
 import ProjectSetup from 'components/projects/[id]/edit';
 import Template from 'layouts/dashboardLayout';
+import { BackToProjectsButton } from 'layouts/projectLayout';
 import { getProjectContext } from 'lib/middleware';
 
 export const getServerSideProps: GetServerSideProps = async context => {
@@ -14,6 +15,9 @@ export const getServerSideProps: GetServerSideProps = async context => {
 export default function SetupPage(pageProps: { user: DashboardUser; project?: Project }) {
   return (
     <Template {...pageProps} selectedMenuItem='projects' title='Projects'>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <BackToProjectsButton />
+      </div>
       <ProjectSetup user={pageProps.user} project={pageProps.project} successPath={() => '/projects'} />
     </Template>
   );
