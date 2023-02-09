81f7fc714f1e31c9e8ac324a1da6bc554c0b57cc
Matt Casey <mattwad@gmail.com>
2023-02-09 14:38:16 -0700

    add instructions to spreadsheet

--- lib/inventory/getProjectInventoryExport.ts
+++ lib/inventory/getProjectInventoryExport.ts
@@ -51,9 +51,47 @@ const greyFill = {
 function getExportFile(data: ProjectData, products: SingleUseProduct[]) {
   const workbook = new ExcelJS.Workbook();
 
+  addInstructionsSheet(workbook);
   addSingleUseSheet(workbook, data, products);
 
   return workbook;
+}
+
+const instructionsText = `
+This spreadsheet is a record of your single-use purchasing. Use it to update your purchase history and compare it to your forecasted purchasing and project goals.
+
+Enter your data in the “Single Use Items” sheet. Records are grouped by date. You can duplicate a group, change its date and add as many records as you would like.
+
+Once you have finished updating your records, upload the sheet to see a customized impact report showing the projected cost savings, the return on investment, the break-even period, and the avoided greenhouse gas emissions and waste prevented.
+`;
+
+const warningText = `Note: This action will replace all existing records, you can use it to fix or remove old records as well as add new ones.`;
+
+function addInstructionsSheet(workbook: ExcelJS.Workbook) {
+  const sheet = workbook.addWorksheet('Instructions', {});
+
+  const firstColumn = sheet.getColumn(1);
+  firstColumn.width = 120;
+
+  // Add first row
+  const row = sheet.addRow(['Chart Reuse by Upstream - Purchase Updates']);
+  row.height = 36;
+  const firstCell = row.getCell(1);
+  firstCell.alignment = { vertical: 'middle' };
+  firstCell.font = { size: 16, bold: true, color: { argb: 'ffffff' } };
+  firstCell.fill = {
+    type: 'pattern',
+    pattern: 'solid',
+    fgColor: { argb: '2bbe50' }
+  };
+
+  const instructionsRow = sheet.addRow([instructionsText]);
+  instructionsRow.alignment = { wrapText: true };
+  instructionsRow.font = { size: 14 };
+  const lastRow = sheet.addRow([warningText]);
+  lastRow.height = 24;
+  lastRow.alignment = { vertical: 'middle', wrapText: true };
+  lastRow.font = { size: 14, bold: true };
 }
 
 // Define Single-Use Worksheet
@@ -82,14 +120,15 @@ function addSingleUseSheet(workbook: ExcelJS.Workbook, data: ProjectData, produc
     { header: 'Row id', key: 'id', width: 40, style: greyFill },
     { header: 'Category', key: 'categoryName', width: 40, style: greyFill },
     { header: 'Description', key: 'title', width: 40, style: greyFill },
-    { header: ['', 'Units per case'], key: 'unitsPerCase', width: 10, style: greyFill },
-    { header: ['', 'Case cost'], key: 'caseCost', width: 10, style: greyFill },
-    { header: ['Baseline', 'Cases Purchased'], key: 'casesPurchased', width: 10, style: greyFill }
+    { header: ['', 'Units per case'], key: 'unitsPerCase', width: 15, style: greyFill },
+    { header: ['', 'Case cost'], key: 'caseCost', width: 15, style: greyFill },
+    { header: ['Baseline', 'Cases Purchased'], key: 'casesPurchased', width: 15, style: greyFill }
   ];
 
   // merge baseline columns
   sheet.mergeCells(1, 4, 1, 6);
   sheet.getCell(1, 4).alignment = { horizontal: 'center' };
+  sheet.getCell(1, 4).protection = { locked: true };
 
   dates.forEach(date => {
     const columnName = `${date.toLocaleDateString()}`;
@@ -98,6 +137,7 @@ function addSingleUseSheet(workbook: ExcelJS.Workbook, data: ProjectData, produc
     const lastCol = columns.push({ header: [columnName, 'Cases purchased'], key: `${date}-casesPurchased`, width: 10 });
     sheet.mergeCells(1, firstCol, 1, lastCol);
     sheet.getCell(1, firstCol).alignment = { horizontal: 'center' };
+    sheet.getCell(1, firstCol).protection = { locked: true };
   });
 
   sheet.columns = columns;
