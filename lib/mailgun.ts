8b72b25da6afc4c4d9d6318e9b7746c690b5efaa
mattcasey <mattwad@gmail.com>
2023-07-28 11:56:02 -0600

    dont require mailgun key to start app

--- lib/mailgun.ts
+++ lib/mailgun.ts
@@ -5,12 +5,12 @@ const API_KEY = process.env.MAILGUN_API_KEY as string;
 const DOMAIN = process.env.MAILGUN_DOMAIN as string;
 
 const mailgun = new Mailgun(formData);
-const client = mailgun.client({ username: 'api', key: API_KEY });
 
-export default client;
+type MailgunClient = ReturnType<typeof mailgun.client>;
 
-type MailgunMessageData = Parameters<typeof client.messages.create>[1];
+type MailgunMessageData = Parameters<MailgunClient['messages']['create']>[1];
 
 export function sendEmail(data: MailgunMessageData) {
+  const client = mailgun.client({ username: 'api', key: API_KEY });
   return client.messages.create(DOMAIN, data);
 }
